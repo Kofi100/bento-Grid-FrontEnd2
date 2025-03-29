a2a845e74d483c288cccb2a749ac898c62c379ec
Kofi K <kofi.k.otchere@gmail.com>
2025-03-29 14:23:57 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section className='card-grid'>
+        <div className='item1'> <p>Create and schedule  content <i>quicker.</i></p> <img src="./images/illustration-create-post.webp" alt="" /></div>
+        <div className='item2'>
+          <p className='item2-title'>Social Media <span>10x</span> <br /> <i>Faster</i> with AI</p> 
+          <img src="./images/illustration-five-stars.webp" alt="" />
+        <p className='item2-littleText'>Over 4,000 5-star reviews</p></div>
+        <div className='item3'>
+          <p className='item3-title'>Schedule to social media.</p>
+          <img src="./images/illustration-schedule-posts.webp" alt="" />
+          <p>Optimize post timings to publish content at the perfect time for your audience. </p></div>
+         <div className='item4'><p className='title'>Write your content using AI.</p> <img src="./images/illustration-ai-content.webp" alt="" /></div>
+        <div className='item5'>
+          <img src="./images/illustration-multiple-platforms.webp" alt="" />
+          <p className='title'>Manage multiple accounts and platforms.</p>
+        </div>
+        <div className='item6'>
+          <p className='title'>Maintain a consistent posting schedule.</p>
+          <img src="./images/illustration-consistent-schedule.webp" alt="" />
+        
+        </div>
+        <div className='item7'>
+          <p><p className='titleBigger'>{'>'}56% </p>faster audience growth</p>
+          <img src="./images/illustration-audience-growth.webp" alt="" />
+        </div>
+        <div className='item8'>
+          <img src="./images/illustration-grow-followers.webp" alt="" />
+          <p className='title'>Grow followers with non-stop content.</p>
+        </div>
+        
+      </section>
+
+
+
+      <section className='sectionMain-First'>
+        
+       
+      </section>
+      <section className='sectionMain-Second'>
+        <section></section>
+        <section>
+
+          
+        </section>
+      </section>
+     
+
+
+
+
+
+
+
+  
+  
+
+  
+ 
+
+  
+  
+  <div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="#">Kofi K</a>.
+  </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/bento-Grid-FrontEnd2", // Ensure this matches your repo name
+});
